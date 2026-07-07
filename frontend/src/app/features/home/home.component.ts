46e629074269b509f21c41d77ff11f8ef5f706a3
Ruth <r0583215031@gmail.com>
2026-07-07 08:24:53 +0300

    FAB-44: fix code review findings - loading state for home, consistent isUser check

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import { UserRole } from '../../core/constants';
 import { AuthService } from '../../core/services/auth.service';
 import { CardComponent } from '../../shared/components/card/card.component';
 import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
@@ -14,5 +13,4 @@ import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner
 })
 export class HomeComponent {
   readonly auth = inject(AuthService);
-  readonly UserRole = UserRole;
 }
