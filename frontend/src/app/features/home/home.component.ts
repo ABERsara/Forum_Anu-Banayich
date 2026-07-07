4fc13b67e8a1f8e25c7fcd742b87e38ff395be6d
Ruth <r0583215031@gmail.com>
2026-07-07 08:17:16 +0300

    FAB-44: show loading spinner while user profile loads on home

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -4,10 +4,11 @@ import { RouterLink } from '@angular/router';
 import { UserRole } from '../../core/constants';
 import { AuthService } from '../../core/services/auth.service';
 import { CardComponent } from '../../shared/components/card/card.component';
+import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CardComponent, RouterLink],
+  imports: [CardComponent, RouterLink, LoadingSpinnerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
